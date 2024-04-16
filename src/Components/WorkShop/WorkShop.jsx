b1dc07710427516fd68412e34fb4ebbe2d98b4fe
Shubhojit Halder <shubhojit_h.ece2020@msit.edu.in>
2024-04-16 18:32:05 +0530

    Workshop styled

--- src/Components/WorkShop/WorkShop.jsx
+++ src/Components/WorkShop/WorkShop.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyledContainer } from "./WorkShop.styled";
+import { StyledContainer } from "./Workshops.styled";
 import { Card2 } from "../Cards/Card2";
 
 const WorkShop = () => {
