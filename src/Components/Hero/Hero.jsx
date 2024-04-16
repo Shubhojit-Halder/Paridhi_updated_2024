4f2d0cd7d8adbf77a509b6d9fa22a627f4a9f66e
Shubhojit Halder <shubhojit_h.ece2020@msit.edu.in>
2024-04-16 17:49:05 +0530

    First push

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,195 @@
+import { useState, useEffect, useRef } from "react";
+import baffle from "baffle";
+import {
+  faFacebook,
+  faInstagram,
+  faLinkedin,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+
+import {
+  PreloaderVideoWrapper,
+  HeroMain,
+  CountdownContainer,
+  CountdownItem,
+  Title,
+  Typewriter,
+  HeroText,
+  HeroContainer,
+  CountText,
+  Footer,
+  Middle,
+  Btn,
+  FontAwesomeIconStyled,
+  DrippingDigit,
+  StyledContainer,
+  HT1,
+} from "./Hero.styled.js";
+import BackgroundAnimation from "../BackGroundAnimation/BackgroundAnimation.jsx";
+
+export const CountdownTimer = ({ targetDate }) => {
+  const calculateTimeLeft = () => {
+    const now = new Date();
+    const difference = targetDate - now;
+
+    if (difference > 0) {
+      const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+      setTimeLeft({
+        days,
+        hours,
+        minutes,
+        seconds,
+      });
+    } else {
+      setTimeLeft({
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      });
+    }
+  };
+
+  const [timeLeft, setTimeLeft] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      calculateTimeLeft();
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [targetDate]);
+
+  return (
+    <CountdownContainer>
+      <CountdownItem>
+        <DrippingDigit>
+          {timeLeft.days <= 9 ? "0" + timeLeft.days : timeLeft.days}
+          <CountText>Days</CountText>
+        </DrippingDigit>
+      </CountdownItem>
+
+      <CountdownItem>
+        <DrippingDigit>
+          {timeLeft.hours <= 9 ? "0" + timeLeft.hours : timeLeft.hours}
+          <CountText>Hours</CountText>
+        </DrippingDigit>
+      </CountdownItem>
+
+      <CountdownItem>
+        <DrippingDigit>
+          {timeLeft.minutes <= 9 ? "0" + timeLeft.minutes : timeLeft.minutes}
+          <CountText>Minutes</CountText>
+        </DrippingDigit>
+      </CountdownItem>
+
+      <CountdownItem>
+        <DrippingDigit style={{ color: "#e70000" }}>
+          <span>
+            {timeLeft.seconds <= 9 ? "0" + timeLeft.seconds : timeLeft.seconds}
+          </span>
+          <CountText>Seconds</CountText>
+        </DrippingDigit>
+      </CountdownItem>
+    </CountdownContainer>
+  );
+};
+
+const Hero = () => {
+  const targetDate = new Date("2024-05-01T23:59:59");
+
+  const [videoPlayed, setVideoPlayed] = useState(false);
+  const [showContent, setShowContent] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVideoPlayed(true);
+    }, 2000); // Play video for 3 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      <StyledContainer loaded={showContent ? "true" : "false"}>
+        <HeroMain>
+          <HeroContainer>
+            <div className="light-source" />
+            <Title>
+              <Typewriter className="baffle" style={{fontFamily:"MyFont",textAlign:"center",width:"100%"}}>
+                Paridhi <span style={{ color: "#e70000" }}>{"'24"}</span>
+              </Typewriter>
+            </Title>
+            <HT1
+              id="HT1"
+              style={{
+                
+              }}
+            >
+              The Annual{" "}
+              <span style={{ color: "#ce0000" }}> technical fest</span> of MSIT
+            </HT1>
+            <HeroText
+              style={{
+                marginBottom: "40px",
+                textTransform: "uppercase",
+                fontSize: "1rem",
+              }}
+            >
+              See you on{" "}
+              <span style={{ color: "#e70000" }}>10th, 11th, 12th</span> May
+            </HeroText>
+            <div className="timer-div">
+              <HeroText
+                style={{
+                  marginBottom: "-40px",
+                  textTransform: "uppercase",
+                  fontSize: "1rem",
+                }}
+              >
+                Registration Begins in
+              </HeroText>
+              <CountdownTimer targetDate={targetDate} />
+            </div>
+          </HeroContainer>
+          <Footer>
+            <Middle>
+              <Btn href="https://www.facebook.com/msitparidhi2023" target="_">
+                <FontAwesomeIconStyled icon={faFacebook} />
+              </Btn>
+              <Btn
+                href="https://www.instagram.com/megatronix__msit/"
+                target="_"
+              >
+                <FontAwesomeIconStyled icon={faInstagram} />
+              </Btn>
+              <Btn
+                href="https://www.linkedin.com/company/msitmegatronix/about/"
+                target="_"
+              >
+                <FontAwesomeIconStyled icon={faLinkedin} />
+              </Btn>
+              <Btn href="https://www.youtube.com/@megatronixmsit921" target="_">
+                <FontAwesomeIconStyled icon={faYoutube} />
+              </Btn>
+            </Middle>
+          </Footer>
+        </HeroMain>
+      </StyledContainer>
+      )}
+    </>
+  );
+};
+
+export default Hero;
---
+++ src/Components/Hero/Hero.styled.js
@@ -0,0 +1,317 @@
+import styled, { keyframes, css } from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export const StyledContainer = styled.div`
+  /* background-image: url("https://images.unsplash.com/photo-1603366615917-1fa6dad5c4fa?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+  background-size: cover;
+  background-position: center; */
+
+  height: 100vh;
+  width: 100%;
+  opacity: ${({ loaded }) => (loaded ? 1 : 0)};
+  transition: opacity 2s ease-in;
+  position: fixed;
+
+  ${({ loaded }) =>
+    !loaded &&
+    css`
+      visibility: hidden;
+    `};
+`;
+
+export const PreloaderVideoWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 9999;
+`;
+
+export const HeroMain = styled.div`
+  height: 100vh;
+  overflow-y: hidden;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+
+  @media (max-width: 600px) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+export const HeroContainer = styled.div`
+  /* height: 90vh; */
+  width: 100%;
+  margin-top: 150px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  justify-content: space-evenly;
+  padding: 0 0 10% 0;
+  @media (max-width: 450px) {
+    #HT1 {
+      font-size: 0.7rem;
+    }
+  }
+`;
+
+export const Title = styled.div`
+  text-align: center;
+  font-size: 3rem;
+  font-weight: 900;
+`;
+
+export const HeroText = styled.div`
+  /* position: relative; */
+  z-index: 1;
+  font-size: 1.2rem;
+  font-weight: bold;
+  text-align: center;
+  /* margin-bottom: -30px; */
+  @media (max-width: 600px) {
+    font-weight: 800;
+    padding: 3%;
+  }
+`;
+
+export const HT1 = styled.div`
+  text-transform: uppercase;
+  font-family: "MyFont";
+  letter-spacing: 0.2em;
+  font-size: 0.9rem;
+  margin-bottom: 40px;
+  @media (max-width: 450px) {
+    font-size: 0.7rem;
+  }
+`;
+
+export const Typewriter = styled.div`
+  font-size: 100%;
+  /* text-shadow: 0 0 20px rgb(255, 0, 0); */
+  font-family: "MyFont";
+  position: relative;
+  margin: 0;
+  color: white;
+  /* font-weight: 600; */
+  position: relative;
+  text-transform: uppercase;
+  text-align: center;
+  display: flex;
+  width: 100%;
+  letter-spacing: 0.2em;
+  &:before {
+    z-index: 0;
+    content: "";
+    position: absolute;
+    width: 200px;
+    height: 3px;
+    left: 25%;
+    margin-top: -15px;
+    background: red;
+  }
+  #pari {
+    font-size: 5rem;
+  }
+  .paridhi-insider {
+    /* margin-top: 20px; */
+    #p1 {
+      font-size: 2.5rem;
+    }
+    #p2 {
+      /* margin-left: -10px; */
+      text-shadow: 0 0 2px rgba(255, 0, 0, 0.75);
+      color: rgb(231, 0, 0);
+      /* margin-top: -20px; */
+      font-size: 1.8rem;
+    }
+    &:before {
+      z-index: 0;
+      content: "";
+      position: absolute;
+      width: 200px;
+      height: 3px;
+      left: 25%;
+      margin-top: -10px;
+      background: red;
+    }
+    @media (max-width: 450px) {
+      font-size: 2.5rem;
+
+      #p1 {
+        margin-top: 5px;
+      }
+      /* font-size :5rem ; */
+      #p2 {
+        margin-top: -13px;
+      }
+      /* } */
+    }
+  }
+  @media (max-width: 450px) {
+    font-size: 2.5rem;
+  }
+`;
+
+export const CountdownContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10%;
+  font-family: "Arial", sans-serif;
+  text-align: center;
+  margin-top: 50px;
+  width: 37.5rem;
+  height: 30%;
+  @media (max-width: 37.5rem) {
+    width: 350px;
+    gap: 5%;
+  }
+  @media (max-width: 28.125rem) {
+    width: 18.75rem;
+  }
+`;
+
+export const CountdownItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 30%;
+  font-size: 5rem;
+  @media (max-width: 600px) {
+    font-size: 2rem;
+  }
+  @media (max-width: 450px) {
+    /* font-size: 1rem; */
+    width: 300px;
+  }
+`;
+
+export const CountText = styled.div`
+  font-size: 15px;
+  font-weight: 500;
+  @media (max-width: 600px) {
+    font-size: 10px;
+    font-weight: 800;
+  }
+`;
+
+export const DrippingDigit = styled.div`
+  background-color: black;
+  text-align: center;
+  /* font-size: 6vw; */
+  font-size: 3rem;
+  font-weight: 400;
+  margin: 0 2.5%;
+  color: white;
+  padding: 10px;
+  width: 6.25rem;
+  border-radius: 20px;
+  border-width: 50px;
+  box-sizing: border-box;
+  /* box-shadow: 0 0.5rem 0.4rem 0.01rem hsl(70, 0%, 19%); */
+  box-shadow: 0 0.2rem 0.9rem 0.01rem #8a000091;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  @media (max-width: 600px) {
+    font-size: 2rem;
+  }
+  @media (max-width: 450px) {
+    width: 80px;
+
+    font-size: 1.5rem;
+  }
+`;
+
+export const effect = keyframes`
+  0% {
+    left: -110%;
+    top: 90%;
+    background: #9f9c9c;
+  }
+  50% {
+ 
+    background: #9f9c9c;
+  }
+  90% {
+  
+    background: #9f9c9c;
+  }
+  100%{
+  
+    background: transparent;
+  }
+`;
+
+export const Footer = styled.footer`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  bottom: 2%;
+`;
+
+export const Middle = styled.div`
+  top: 50%;
+  transform: translateY(13%);
+  width: 100%;
+  text-align: center;
+  display: flex;
+  @media (max-width: 600px) {
+    transform: translate(-3%);
+  }
+`;
+
+export const Btn = styled.a`
+  display: inline-block;
+  width: 40px;
+  height: 40px;
+  display: flex;
+  background-color: #1f1f1f5a;
+  margin: 1rem;
+  border-radius: 30%;
+  /* box-shadow: 0 0 5px 5px #ffffff1a; */
+  color: #fff;
+  overflow: hidden;
+  position: relative;
+  text-decoration: none;
+
+  &:hover i {
+    transform: scale(1.3);
+    color: #f1f1f1;
+  }
+
+  &:before {
+    content: "";
+    position: absolute;
+    width: 170%;
+    height: 20%;
+    transform: rotate(45deg);
+    left: 100%;
+    top: 90%;
+  }
+
+  &:hover::before {
+    animation: ${effect} 0.3s 1;
+    top: -10%;
+    left: -10%;
+  }
+`;
+
+export const FontAwesomeIconStyled = styled(FontAwesomeIcon)`
+  font-size: 20px;
+  padding: 10px;
+`;
